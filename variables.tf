438f96b3bac169142d89867450ddd1f0a907d63e
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-12-11 16:52:09 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "workspace" {}
 
-variable "slack_channel_name" {}
-variable "encrypted_slack_token" {}
+variable "slack_channel_email" {}
 variable "gcp_folder_id" {}
 variable "github_org_name" {}
 variable "github_repository_name" {}
